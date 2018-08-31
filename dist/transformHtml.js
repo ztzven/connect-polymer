0912749fa69eb9f9ab112be3f8f757db8f140cdb
Steven French <steven@speakdata.co.nz>
2018-08-31 15:22:26 +1200

    add dist

---
+++ dist/transformHtml.js
@@ -0,0 +1,29 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var browserCapabilities = require("browser-capabilities");
+var polymerBuild = require("polymer-build");
+var getCompileTarget = require("polyserve/lib/get-compile-target.js");
+function transformHtml(userAgent, content, packageName, absolutePath, npm, rootDir, componentDir) {
+    var capabilities = browserCapabilities.browserCapabilities(userAgent);
+    var compileTarget = getCompileTarget.getCompileTarget(capabilities, "auto");
+    var options = {
+        compileTarget: compileTarget,
+        transformModules: !capabilities.has("modules"),
+    };
+    return polymerBuild.htmlTransform(content, {
+        injectBabelHelpers: options.transformModules ? "full" : "none",
+        injectRegeneratorRuntime: options.transformModules,
+        injectAmdLoader: options.transformModules,
+        js: {
+            compile: options.compileTarget,
+            transformModulesToAmd: options.transformModules ? "auto" : false,
+            moduleResolution: npm ? "node" : "none",
+            filePath: absolutePath,
+            isComponentRequest: false,
+            packageName: packageName,
+            componentDir: componentDir,
+            rootDir: rootDir
+        }
+    });
+}
+exports.transformHtml = transformHtml;
---
+++ dist/transformJs.js
@@ -0,0 +1,24 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var browserCapabilities = require("browser-capabilities");
+var polymerBuild = require("polymer-build");
+var getCompileTarget = require("polyserve/lib/get-compile-target.js");
+function transformJs(userAgent, content, packageName, absolutePath, npm, rootDir, componentDir) {
+    var capabilities = browserCapabilities.browserCapabilities(userAgent);
+    var compileTarget = getCompileTarget.getCompileTarget(capabilities, "auto");
+    var options = {
+        compileTarget: compileTarget,
+        transformModules: !capabilities.has("modules"),
+    };
+    return polymerBuild.jsTransform(content, {
+        compile: options.compileTarget,
+        transformModulesToAmd: options.transformModules ? "auto" : false,
+        moduleResolution: npm ? "node" : "none",
+        filePath: absolutePath,
+        isComponentRequest: false,
+        packageName: packageName,
+        componentDir: componentDir,
+        rootDir: rootDir
+    });
+}
+exports.transformJs = transformJs;
